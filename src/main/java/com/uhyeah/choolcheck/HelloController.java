2106e733d422dfc8bec1b50ae87cdca704b5f59d
yevini118 <79689350+yevini118@users.noreply.github.com>
2023-01-20 17:24:11 +0900

    Update HelloController.java

--- src/main/java/com/uhyeah/choolcheck/HelloController.java
+++ src/main/java/com/uhyeah/choolcheck/HelloController.java
@@ -1,13 +1,16 @@
 package com.uhyeah.choolcheck;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class HelloController {
 
     @GetMapping("/hello")
     public String HelloAPI() {
+        System.out.println("Test Success");
         return "Test Success";
     }
 
