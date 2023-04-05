9c81b18749b01c8494a7693d1d0b28f55294a317
yevini118 <79689350+yevini118@users.noreply.github.com>
2023-04-05 16:03:48 +0900

    Update StatisticsService.java

--- src/main/java/com/uhyeah/choolcheck/web/statistics/StatisticsService.java
+++ src/main/java/com/uhyeah/choolcheck/web/statistics/StatisticsService.java
@@ -51,14 +51,10 @@ public class StatisticsService {
     public long calculateTime(LocalTime startTime, LocalTime endTime) {
 
         if (endTime.isBefore(startTime)) {
-            System.out.println(Duration.between(startTime, LocalTime.MIDNIGHT).toHours());
-
-            System.out.println(Duration.between(LocalTime.MIDNIGHT, endTime).toHours());
-            System.out.println(startTime);
-            return Duration.between(startTime, LocalTime.MIDNIGHT).toMinutes() + Duration.between(LocalTime.MIDNIGHT, endTime).toMinutes();
+            return Duration.between(startTime, endTime).toHours() + 24;
         }
         else {
-            return Duration.between(startTime, endTime).toMinutes();
+            return Duration.between(startTime, endTime).toHours();
         }
     }
 }
