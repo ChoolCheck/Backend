79d22897fb1bf88663f4658b0ef80b8d63f85045
yevini118 <79689350+yevini118@users.noreply.github.com>
2023-04-08 14:55:41 +0900

    Update site url

--- src/main/java/com/uhyeah/choolcheck/global/SiteProperties.java
+++ src/main/java/com/uhyeah/choolcheck/global/SiteProperties.java
@@ -3,5 +3,5 @@ package com.uhyeah.choolcheck.global;
 
 public class SiteProperties {
 
-    public static final String SITE_URL = "http://choolcheck-front.s3-website.ap-northeast-2.amazonaws.com";
+    public static final String SITE_URL = "https://choolcheck.site";
 }
